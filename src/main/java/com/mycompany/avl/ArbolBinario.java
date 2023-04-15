00152a93535d7a12681a69c7582e30d40a7f572a
alexo <alexo@192.168.0.42>
2023-04-14 23:21:54 -0600

    Metodo para eliminar nodo arbol y devolver nuevo nodo

--- src/main/java/com/mycompany/avl/ArbolBinario.java
+++ src/main/java/com/mycompany/avl/ArbolBinario.java
@@ -82,4 +82,76 @@ public class ArbolBinario {
         }
         return aux;
     }
+    //Metodo para eliminar un nodo del Arbol
+    public boolean eliminar(int d){
+        NodoArbol auxiliar=raiz;
+        NodoArbol padre=raiz;
+        boolean esHijoIzq=true;
+        while(auxiliar.dato!=d){
+            padre=auxiliar;
+            if(d<auxiliar.dato){
+                esHijoIzq=true;
+                auxiliar=auxiliar.hijoIzquierdo;
+            }else{
+                esHijoIzq=false;
+                auxiliar=auxiliar.hijoDerecho;
+            }
+            if(auxiliar==null){
+                return false;
+            }
+        }//Fin del while
+        if(auxiliar.hijoIzquierdo==null && auxiliar.hijoDerecho==null){
+            if(auxiliar==raiz){
+                raiz=null;
+            }else if(esHijoIzq){
+                padre.hijoIzquierdo=null;
+            }else{
+                padre.hijoDerecho=null;
+            }
+        }else if(auxiliar.hijoDerecho==null){
+            if(auxiliar==raiz){
+                raiz=auxiliar.hijoIzquierdo;
+            }else if(esHijoIzq){
+                padre.hijoIzquierdo=auxiliar.hijoIzquierdo;
+            }else{
+                padre.hijoDerecho=auxiliar.hijoIzquierdo;
+            }
+        }else if(auxiliar.hijoIzquierdo==null){
+            if(auxiliar==raiz){
+                raiz=auxiliar.hijoDerecho;
+            }else if(esHijoIzq){
+                padre.hijoIzquierdo=auxiliar.hijoDerecho;
+            }else{
+                padre.hijoDerecho=auxiliar.hijoIzquierdo;
+            }
+        }else{
+            NodoArbol reemplazo=obtenerNodoReemplazo(auxiliar);
+            if(auxiliar==raiz){
+                raiz=reemplazo;
+            }else if{
+                padre.hijoIzquierdo=reemplazo;
+            }else{
+                padre.hijoDerecho=reemplazo;
+            }
+            reemplazo.hijoIzquierdo=auxiliar.hijoIzquierdo;
+        }
+        return true; 
+    }
+    //Metodo encargado de devolvernos el nodo reemplazo
+    public NodoArbol obtenerNodoReemplazo(NodoArbol nodoReemp){
+        NodoArbol reemplazarPadre=nodoReemp;
+        NodoArbol reemplazo=nodoReemp;
+        NodoArbol auxiliar=nodoReemp.hijoDerecho;
+        while(auxiliar!=null){
+            reemplazarPadre=reemplazo;
+            reemplazo=auxiliar;
+            auxiliar=auxiliar.hijoIzquierdo;
+        }
+        if(reemplazo!=nodoReemp.hijoDerecho){
+            reemplazarPadre.hijoIzquierdo=reemplazo.hijoDerecho;
+            reemplazo.hijoDerecho=nodoReemp.hijoDerecho;
+        }
+        System.out.println("El nodo reemplazo es " + reemplazo);
+        return reemplazo;
+    }
 }
